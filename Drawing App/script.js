466ec856b6bc3967eb0500c45d32dfff1affa891
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2024-10-23 23:40:07 +0300

    deleting extra spacing

--- Drawing App/script.js
+++ Drawing App/script.js
@@ -26,7 +26,6 @@ window.addEventListener("load", () => {
 const drawRect = (e) => {
    
     if(!fillColor.checked) {
-       
         return ctx.strokeRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY);
     }
     ctx.fillRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY);
